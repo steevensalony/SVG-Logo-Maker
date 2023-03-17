b7ef215b4580a68ab8b7a27f73801274481b0bbe
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-03-16 21:06:26 -0400

    Added test for triangle class

---
+++ tests/triangle.test.js
@@ -0,0 +1,12 @@
+const triangles = require('../library/triangle.js');
+
+// Testing for shape color
+describe('Triangle', () => {
+  describe('render', () => {
+    it('should return the shape color for the triangle shape that the user provides', () => {
+      const shape = new triangles;
+      shape.setShapeColor('blue');
+      expect(shape.renderShape()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
+    })
+  })
+})
