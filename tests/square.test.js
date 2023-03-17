fd3ae2fa2a72570fc1acced322703bc9d2f6b48a
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-03-16 21:03:31 -0400

    Added test for square class

---
+++ tests/square.test.js
@@ -0,0 +1,12 @@
+const squares = require('../library/square.js');
+
+// Testing for shape color
+describe('Square', () => {
+  describe('render', () => {
+    it('should return the shape color for the square shape that the user provides', () => {
+      const shape = new squares;
+      shape.setShapeColor('blue');
+      expect(shape.renderShape()).toEqual('<rect width="200" height="200" stroke="black" fill="blue" stroke-width="5"/>');
+    })
+  })
+})
