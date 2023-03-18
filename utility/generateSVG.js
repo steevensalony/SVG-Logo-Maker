bfce6612b5fc7e421d5d4aa94c535d80b2d214fc
Steeven Salony <steeven@imac.myfiosgateway.com>
2023-03-18 10:17:39 -0400

    Fixed triangle shape and exports

--- utility/generateSVG.js
+++ utility/generateSVG.js
@@ -1,24 +1,24 @@
-const shapes = require('../library/shape.j')
+const shapes = require('../library/shape.js')
 const circles = require('../library/circle.js')
 const triangles = require('../library/triangle.js')
 const squares = require('../library/square.js')
 
 function generateSVG(data) {
-  let newStr = '';
+  let newShapes = '';
   const newShape = new shapes;
-  newStr = newShape.render();
+  newShapes = newShape.render();
 
   if (data.shape === 'Circle') {
     const newCircle =  new circles(data.text, data.textColor, data.shapeColor);
-    newStr += `\n    ${newCircle.renderShape()}\n    ${newCircle.renderText()}`
+    newShapes += `\n${newCircle.renderShape()}\n${newCircle.renderText()}`
   } else if (data.shape === 'Triangle') {
     const newTriangle = new triangles(data.text, data.textColor, data.shapeColor);
-    newStr += `\n    ${newTriangle.renderShape()}\n    ${newTriangle.renderText()}`
+    newShapes += `\n${newTriangle.renderShape()}\n${newTriangle.renderText()}`
   } else if (data.shape === 'Square') {
     const newSquare = new squares(data.text, data.textColor, data.shapeColor);
-    newStr += `\n    ${newSquare.renderShape()}\n    ${newSquare.renderText()}`
+    newShapes += `\n${newSquare.renderShape()}\n${newSquare.renderText()}`
   }
-  return newStr;
+  return newShapes;
 }
 
 module.exports = generateSVG;
